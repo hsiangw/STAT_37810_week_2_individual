5be01cb7cf30a108f6e7ccc2f735e41da3970091
hsiangw <hsiangw@uchicago.edu>
2020-10-11 16:16:47 -0500

    problem 1

--- buggy_code.R
+++ buggy_code.R
@@ -6,7 +6,6 @@ investment_value<-function(x,y,z=0.95, different_years=c(3,5)){
   different_multiplier = 1 + z
   beginning_x=x
   for(i in 1:10){
-    i=i+1
     if (!(i %in% different_years)){      # should be true unless i is in the different_years vector
       x<-beginning_x*multiplier
     } else {
@@ -16,3 +15,12 @@ investment_value<-function(x,y,z=0.95, different_years=c(3,5)){
   }
   return(x) #return value
 }
+
+#a
+#The investment rate is 0. Therefore, the final value is same as initial value.
+
+#b
+investment_value(10,0,0)
+investment_value(10,0.05,0)
+investment_value(10,0,-0.05)
+investment_value(10,0.05,-0.05)
